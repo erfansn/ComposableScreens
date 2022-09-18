5cb205af85d754eb32d8848cb4cfb24c80bba03d
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2022-09-18 15:41:03 +0430

    Change HomeScreen of travel ui to ComposableScreensNavHost

--- app/src/main/java/ir/erfansn/composablescreens/MainActivity.kt
+++ app/src/main/java/ir/erfansn/composablescreens/MainActivity.kt
@@ -3,42 +3,18 @@ package ir.erfansn.composablescreens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
-import ir.erfansn.composablescreens.travel.home.HomeScreen
-import ir.erfansn.composablescreens.travel.ui.theme.TravelOneTheme
+import ir.erfansn.composablescreens.ui.navigation.ComposableScreensNavHost
 import ir.erfansn.composablescreens.ui.theme.ComposableScreensTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
         setContent {
             ComposableScreensTheme {
-                // A surface container using the 'background' color from the theme
-                TravelOneTheme {
-                    HomeScreen()
-                }
+                ComposableScreensNavHost()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposableScreensTheme {
-        Greeting("Android")
     }
 }
