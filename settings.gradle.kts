79f549f3171db22e31647be405b8aab0bdec4511
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2022-09-24 20:35:55 +0330

    Add dependency Material3 to Catalog Version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,14 +23,17 @@ dependencyResolutionManagement {
 
             library("androidx-compose-ui", "androidx.compose.ui", "ui").versionRef("compose")
             library("androidx-compose-material", "androidx.compose.material", "material").versionRef("compose")
+            library("androidx-compose-material3", "androidx.compose.material3", "material3").version("1.0.0-beta02")
             library("androidx-compose-material-icons", "androidx.compose.material", "material-icons-extended").versionRef("compose")
             library("androidx-compose-ui-tooling-preview", "androidx.compose.ui", "ui-tooling-preview").versionRef("compose")
+            val composeLibraries = listOf("androidx-compose-ui", "androidx-compose-material-icons", "androidx-compose-ui-tooling-preview")
+            bundle("compose", composeLibraries + "androidx-compose-material")
+            bundle("compose-m3", composeLibraries + "androidx-compose-material3")
 
             library("androidx-compose-ui-tooling", "androidx.compose.ui", "ui-tooling").versionRef("compose")
             library("androidx-compose.ui-test-manifest", "androidx.compose.ui", "ui-test-manifest").versionRef("compose")
-
-            bundle("compose", listOf("androidx-compose-ui", "androidx-compose-material", "androidx-compose-material-icons", "androidx-compose-ui-tooling-preview"))
             bundle("compose-debug", listOf("androidx-compose-ui-tooling", "androidx-compose.ui-test-manifest"))
+
             library("androidx-activity-compose", "androidx.activity:activity-compose:1.5.1")
             library("androidx-core", "androidx.core:core-ktx:1.8.0")
             library("androidx-constraintlayout-compose", "androidx.constraintlayout:constraintlayout-compose:1.0.1")
