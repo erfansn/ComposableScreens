41b448987cb63a20eee62f47ae50f78db0c8856b
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2024-07-23 20:18:11 +0330

    Add Haze gradle dependency

--- food/build.gradle.kts
+++ food/build.gradle.kts
@@ -41,13 +41,14 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.haze)
 
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.windowSizeClass)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.ui.text.google.fonts)
-    debugImplementation(libs.androidx.compose.ui.tooling.asProvider())
+    debugImplementation(libs.androidx.compose.ui.tooling)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
