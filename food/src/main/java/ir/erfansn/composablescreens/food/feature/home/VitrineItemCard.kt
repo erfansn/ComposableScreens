0296f423b3e4fd18f3e6dc23fd9d4036db99377a
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2024-07-23 20:20:04 +0330

    Apply Glass-morphism to background of price tape

--- food/src/main/java/ir/erfansn/composablescreens/food/feature/home/VitrineItemCard.kt
+++ food/src/main/java/ir/erfansn/composablescreens/food/feature/home/VitrineItemCard.kt
@@ -1,6 +1,7 @@
 package ir.erfansn.composablescreens.food.feature.home
 
 import android.icu.text.NumberFormat
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -11,8 +12,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Card
@@ -21,12 +22,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.buildAnnotatedString
@@ -34,15 +35,52 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.HazeStyle
+import dev.chrisbanes.haze.haze
+import dev.chrisbanes.haze.hazeChild
 import ir.erfansn.composablescreens.food.R
 import ir.erfansn.composablescreens.food.ui.FoodTheme
 import java.util.Locale
+
+val vitrineItems = listOf(
+    VitrineItem(
+        title = "Sea Salt Chocolate Chip Cookie",
+        imageId = R.drawable.peanut_butter_chocolate_chipcookie,
+        backgroundColor = Color.Yellow,
+        priceInCent = 148
+    ),
+    VitrineItem(
+        title = "Sea Salt Chocolate Chip Cookie",
+        imageId = R.drawable.peanut_butter_chocolate_chipcookie,
+        backgroundColor = Color.Blue,
+        priceInCent = 148
+    ),
+    VitrineItem(
+        title = "Sea Salt Chocolate Chip Cookie",
+        imageId = R.drawable.peanut_butter_chocolate_chipcookie,
+        backgroundColor = Color.Cyan,
+        priceInCent = 148
+    ),
+    VitrineItem(
+        title = "Sea Salt Chocolate Chip Cookie",
+        imageId = R.drawable.peanut_butter_chocolate_chipcookie,
+        backgroundColor = Color.DarkGray,
+        priceInCent = 148
+    ),
+    VitrineItem(
+        title = "Sea Salt Chocolate Chip Cookie",
+        imageId = R.drawable.peanut_butter_chocolate_chipcookie,
+        backgroundColor = Color.Red,
+        priceInCent = 148
+    )
+)
 
 typealias Cent = Int
 
 data class VitrineItem(
     val title: String,
-    val image: Painter,
+    @DrawableRes val imageId: Int,
     val backgroundColor: Color,
     val priceInCent: Cent,
 )
@@ -51,32 +89,38 @@ data class VitrineItem(
 fun VitrineItemCard(
     vitrineItem: VitrineItem,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
 ) {
     Card(
         onClick = onClick,
-        shape = MaterialTheme.shapes.large,
+        shape = MaterialTheme.shapes.large.copy(CornerSize(34.dp)),
         colors = CardDefaults.cardColors(containerColor = vitrineItem.backgroundColor),
         modifier = modifier,
     ) {
         Box(
             contentAlignment = Alignment.BottomCenter,
-            modifier = Modifier.aspectRatio(4/5f)
+            modifier = Modifier.aspectRatio(4/4.5f)
         ) {
-            Column {
+            val hazeState = remember { HazeState() }
+            Column(modifier = Modifier.haze(hazeState)) {
                 Text(
                     text = vitrineItem.title,
-                    style = MaterialTheme.typography.headlineMedium,
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                        .padding(top = 12.dp),
-                    fontWeight = FontWeight.Bold
+                    style = MaterialTheme.typography.displaySmall,
+                    modifier = Modifier
+                        .padding(horizontal = 24.dp)
+                        .padding(top = 18.dp)
+                        .weight(2f),
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 3
                 )
                 Image(
-                    painter = vitrineItem.image,
+                    painter = painterResource(id = vitrineItem.imageId),
                     contentDescription = null,
                     contentScale = ContentScale.Crop,
                     alignment = Alignment.TopCenter,
-                    modifier = Modifier.scale(1.2f)
+                    modifier = Modifier
+                        .scale(1.2f)
+                        .weight(3f),
                 )
             }
 
@@ -87,8 +131,14 @@ fun VitrineItemCard(
                     .padding(8.dp)
                     .fillMaxWidth()
                     .clip(CircleShape)
-                    .background(Color.LightGray)
                     .border(1.dp, color = Color.Gray, shape = CircleShape)
+                    .hazeChild(
+                        hazeState,
+                        CircleShape,
+                        HazeStyle(
+                            tint = Color.White.copy(alpha = 0.3f),
+                            blurRadius = 30.dp)
+                    )
                     .padding(4.dp)
             ) {
                 val productPriceByKg = buildAnnotatedString {
@@ -101,7 +151,7 @@ fun VitrineItemCard(
                 }
                 Text(
                     text = productPriceByKg,
-                    modifier = Modifier.padding(start = 16.dp)
+                    modifier = Modifier.padding(start = 16.dp),
                 )
                 Box(
                     contentAlignment = Alignment.Center,
@@ -137,11 +187,10 @@ private fun VitrineCardPreview() {
             vitrineItem = VitrineItem(
                 title = "Peanut butter chocolate chip cookie",
                 priceInCent = 689,
-                image = painterResource(id = R.drawable.peanut_butter_chocolate_chipcookie),
+                imageId = R.drawable.peanut_butter_chocolate_chipcookie,
                 backgroundColor = Color.Yellow
             ),
-            onClick = { },
-            modifier = Modifier.width(240.dp)
+            onClick = { }
         )
     }
 }
