4d348d06d91019317ca14db70db07af87f01c261
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2024-08-13 18:39:51 +0330

    Implement CartStorage

---
+++ food/src/main/java/ir/erfansn/composablescreens/food/data/CartStorage.kt
@@ -0,0 +1,8 @@
+package ir.erfansn.composablescreens.food.data
+
+import androidx.collection.mutableIntIntMapOf as mutableProductIdQuantityMapOf
+
+object CartStorage {
+
+    val storage = mutableProductIdQuantityMapOf()
+}
