9f1a229c375ad817d65d6fb4aac53db4e6f55e4b
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2024-09-04 18:01:40 +0330

    Fix products list wasn't updated

--- food/src/main/java/ir/erfansn/composablescreens/food/feature/cart/CartViewModel.kt
+++ food/src/main/java/ir/erfansn/composablescreens/food/feature/cart/CartViewModel.kt
@@ -1,15 +1,20 @@
 package ir.erfansn.composablescreens.food.feature.cart
 
+import androidx.compose.ui.util.fastSumBy
 import androidx.lifecycle.ViewModel
 import ir.erfansn.composablescreens.food.data.CartStorage
 import ir.erfansn.composablescreens.food.data.ProductProvider
 
 class CartViewModel : ViewModel() {
 
-    val cartProducts = buildList {
+    val cartProducts get() = buildList {
         val productsList = ProductProvider.getProductList()
         CartStorage.storage.forEach { id, quantity ->
             this += productsList.first { it.id == id }.toCartProduct(quantity)
         }
+    }
+
+    val productsTotalPrice get() = cartProducts.fastSumBy {
+        it.totalPrice
     }
 }
