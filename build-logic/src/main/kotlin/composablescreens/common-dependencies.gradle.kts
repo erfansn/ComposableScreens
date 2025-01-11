a1fea5d15471e9cdca42167632a7105a74fc044b
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2025-01-11 18:25:46 +0330

    Consistently apply dependency declaration syntax

--- build-logic/src/main/kotlin/composablescreens/common-dependencies.gradle.kts
+++ build-logic/src/main/kotlin/composablescreens/common-dependencies.gradle.kts
@@ -16,8 +16,6 @@
 
 package composablescreens
 
-import gradle.kotlin.dsl.accessors._6ab6c3f1daf0c54b8463dcc0a8d5f754.implementation
-import gradle.kotlin.dsl.accessors._872ad73bda21390b99a23da2f9228964.debugImplementation
 import libs
 
 plugins {
@@ -25,14 +23,14 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":common"))
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.activity.compose)
+    "implementation"(project(":common"))
+    "implementation"(libs.androidx.core.ktx)
+    "implementation"(libs.androidx.activity.compose)
 
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    debugImplementation(libs.androidx.compose.ui.tooling)
+    "implementation"(platform(libs.androidx.compose.bom))
+    "implementation"(libs.androidx.compose.ui.tooling.preview)
+    "debugImplementation"(libs.androidx.compose.ui.tooling)
 
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.kotlinx.serialization)
+    "implementation"(libs.androidx.navigation.compose)
+    "implementation"(libs.kotlinx.serialization)
 }
